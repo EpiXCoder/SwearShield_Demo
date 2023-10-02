a1782260992c235c27001091efcc5f11b62e7fa0
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-10-02 01:13:50 -0400

    made it pretty

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const { SwearShield } = require('swear-shield');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,12 +10,20 @@ const io = socketIo(server);
 const filter = new SwearShield();
 
 app.use(express.static('public'));
+app.use(bodyParser.json()); 
+
 
 io.on('connection', (socket) => {
     socket.on('new-comment', (data) => {
         const cleanedComment = filter.sanitize(data.comment);
         io.emit('display-comment', { name: data.name, comment: cleanedComment });
     });
+});
+
+app.post('/check-profanity', (req, res) => {
+    const comment = req.body.comment;
+    const hasProfanity = filter.isProfane(comment);
+    res.json({ hasProfanity });
 });
 
 server.listen(3000, () => {
